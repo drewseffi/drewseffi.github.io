586005b951e920b3cc9c2231b00b5942c4d0998d
drew <drewsutherland23@gmail.com>
2025-12-02 01:39:47 +0000

    feat: add slideshow functionality -Add slideshow functionality in javascript with help from W3Schools -Add extra images for parley for slideshow -Minor layout tweaks to font size and padding in intro box

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-// Simple contact form submission message
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Thank you for reaching out! I will get back to you soon.');
-  this.reset();
-});
+// Start reference. Code taken from https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+// End reference
--- notes.md
+++
@@ -1,6 +0,0 @@
-# Notes
-- Remove wall of text (make seperate pages)
-- Find a better design
-- Centre left bar
-- Look for other fonts
-- 70vh height, justify between, name with date on same line, justify between
